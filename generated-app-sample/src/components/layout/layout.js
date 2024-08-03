dc381f52237413bf615d1d44d359819f23af3a73
Т. Жамъянгарав <jay@uniqsoft.tech>
2024-08-03 12:38:49 +0800

    Add layout component

---
+++ generated-app-sample/src/components/layout/layout.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+import { useAddress } from "@thirdweb-dev/react";
+
+function Layout({ children }) {
+  const address = useAddress();
+  return (
+    <div className="flex flex-col min-h-screen bg-white">
+      <header className="flex items-center justify-between w-full px-4 py-3">
+        <div className="flex items-center gap-3">
+          <h1 className="text-xl font-bold text-gray-900">QUICK DAPP</h1>
+        </div>
+        <div className="flex items-center gap-4">
+          <div>haha</div>
+        </div>
+      </header>
+      <main className="container flex-grow p-4 mx-auto text-gray-900">
+        {children}
+      </main>
+      <footer className="flex justify-center p-4 font-bold text-black">
+        <p>Powered By QuickDapp Team 2024</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Layout;
