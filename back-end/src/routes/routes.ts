5539c775f71e6c2df53f2ea03af82a6ec82d54ef
Batgerelt <grey@uniqsoft.tech>
2024-08-03 15:17:58 +0800

    Update routes.ts

--- back-end/src/routes/routes.ts
+++ back-end/src/routes/routes.ts
@@ -1,15 +1,15 @@
 import express from "express";
 import { failed } from "../utils/res.utils";
-import { BlocksoutController } from "../controller/blocksout.controller";
+import { MainController } from "../controller/main.controller";
 
 const router = express.Router();
-const blocksoutController = new BlocksoutController();
+const mainController = new MainController();
 
 router.get("/", (req, res) => {
   failed({ res, err: "Bad Request", status: 400 });
 });
 
 
-router.post("/blocksout/get-abi", blocksoutController.getABI);
+router.post("/build-app", mainController.buildApp);
 
 export default router;
