ee634543ff09cfe8de2c31b0be7b1a7cac64e7eb
Batgerelt <grey@uniqsoft.tech>
2024-08-03 14:12:53 +0800

    Update _app.tsx

--- landing-page/pages/_app.tsx
+++ landing-page/pages/_app.tsx
@@ -1,8 +1,13 @@
 import { AppProps } from "next/app";
 import "../styles/globals.css";
+import { ApiProvider } from "../context/api.context";
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ApiProvider>
+      <Component {...pageProps} />
+    </ApiProvider>
+  );
 }
 
 export default App;
