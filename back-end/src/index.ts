df66faef1e79b4efbf4384d9c03a4e7c87147491
Batgerelt <grey@uniqsoft.tech>
2024-08-03 12:36:52 +0800

    Update index.ts

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,7 +1,19 @@
 import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
+import { failed } from './utils/res.utils';
+import routes from './routes/routes';
+
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json({ limit: '1024mb' }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use((err: any, req: any, res: any, next: any) => failed({ res, err, status: 500, message: "Internal Server Error" }));
+
+app.use("/api", routes);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello from TypeScript Express server!');
