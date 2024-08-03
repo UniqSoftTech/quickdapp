e677dd2ef1487e2a8419fce38282d2f032c2767e
Batgerelt <grey@uniqsoft.tech>
2024-08-03 12:52:08 +0800

    add blocksout/get-abi post end-point

--- back-end/src/routes/routes.ts
+++ back-end/src/routes/routes.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import { failed } from "../utils/res.utils";
+import { BlocksoutController } from "../controller/blocksout.controller";
 
 const router = express.Router();
+const blocksoutController = new BlocksoutController();
 
 router.get("/", (req, res) => {
   failed({ res, err: "Bad Request", status: 400 });
 });
+
+
+router.post("/blocksout/get-abi", blocksoutController.getABI);
 
 export default router;
