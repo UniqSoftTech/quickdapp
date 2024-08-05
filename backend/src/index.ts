3ffdcd1588e4de65bd3ada74f63a6f839c79dcb7
Batgerelt <grey@uniqsoft.tech>
2024-08-05 15:02:44 +0800

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
-import { failed } from './utils/res.utils';
+import { failed, success } from './utils/res.utils';
 import routes from './routes/routes';
 import { PORT } from './config/env.config';
 
@@ -11,13 +11,9 @@ app.use(cors());
 app.use(express.json({ limit: '1024mb' }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use((err: any, req: any, res: any, next: any) => failed({ res, err, status: 500, message: "Internal Server Error" }));
-
 app.use("/api", routes);
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello from TypeScript Express server!');
-});
+app.get('/', (req: Request, res: Response) => success({ res, message: "OK", result: new Date() }));
+app.use((err: any, req: any, res: any, next: any) => failed({ res, err, status: 500, message: "Internal Server Error" }));
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
