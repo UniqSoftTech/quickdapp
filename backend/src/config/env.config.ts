822f32015ddfd63fd58a42e6d4c5cb71ee24c69e
Batgerelt <grey@uniqsoft.tech>
2024-08-05 15:01:30 +0800

    add configs
    - TENDERLY_PROJECT_SLUG
    - TENDERLY_ACCESS_KEY
    - TENDERLY_ACCOUNT_SLUG
    - ALCHEMY_APP_API_KEY

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -3,3 +3,8 @@ dotenv.config();
 
 export const PORT = process.env.PORT as string || 3001;
 export const BLOCKSCOUT_URL = process.env.BLOCKSCOUT_URL as string;
+export const VERCEL_TOKEN = process.env.VERCEL_TOKEN as string;
+export const TENDERLY_PROJECT_SLUG = process.env.TENDERLY_PROJECT_SLUG as string;
+export const TENDERLY_ACCESS_KEY = process.env.TENDERLY_ACCESS_KEY as string;
+export const TENDERLY_ACCOUNT_SLUG = process.env.TENDERLY_ACCOUNT_SLUG as string;
+export const ALCHEMY_APP_API_KEY = process.env.ALCHEMY_APP_API_KEY as string;
