af33f17377dcfb4a869b3b51e0709177e45f96d0
Т. Жамъянгарав <jay@uniqsoft.tech>
2024-08-06 18:25:29 +0800

    Add SEOHead to _app.js page

--- backend/templates/pages/_app.js
+++ backend/templates/pages/_app.js
@@ -4,22 +4,26 @@ import { ChainId } from "@thirdweb-dev/sdk";
 import "../styles/public.css";
 import Layout from "@/components/layout/layout";
 import { Inter } from "next/font/google";
+import SEOHead from "@/components/display/Head";
 
 const activeChainId = ChainId.Mainnet;
 const inter = Inter({ subsets: ["latin"] });
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThirdwebProvider
-      clientId="4af4ad585e326e6e7b9f2c1fd001dee5"
-      desiredChainId={activeChainId}
-    >
-      <main className={inter.className}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </main>
-    </ThirdwebProvider>
+    <>
+      <SEOHead />
+      <ThirdwebProvider
+        clientId="4af4ad585e326e6e7b9f2c1fd001dee5"
+        desiredChainId={activeChainId}
+      >
+        <main className={inter.className}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </main>
+      </ThirdwebProvider>
+    </>
   );
 }
 
