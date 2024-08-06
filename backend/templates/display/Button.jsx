70b2332e15caf5f6484e346ef4a21ed626ba43d2
Т. Жамъянгарав <jay@uniqsoft.tech>
2024-08-06 17:50:54 +0800

    Changed template components route

---
+++ backend/templates/display/Button.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { useAddress, ConnectWallet } from "@thirdweb-dev/react";
+import colors from "@/utils/colors";
+
+function Button({ onClick, title, disabled, loading }) {
+  const address = useAddress();
+
+  if (!address) {
+    return (
+      <ConnectWallet
+        theme="light"
+        modalTitle="QuickDapp"
+        style={{
+          background: colors.main,
+          color: "black",
+          padding: "10px 10px",
+          fontWeight: "bold",
+        }}
+      />
+    );
+  }
+
+  const buttonClasses = `
+    relative rounded px-5 py-2.5 overflow-hidden group transition-all ease-out duration-300 
+    ${
+      disabled || loading
+        ? "bg-neutral-800 text-gray-400 cursor-not-allowed"
+        : "bg-primary hover:bg-gradient-to-r hover:from-yellow-500 hover:to-yellow-400 text-black hover:ring-2 hover:ring-offset-2 hover:ring-yellow-400"
+    }
+  `;
+
+  const spanClasses = `
+    absolute right-0 w-44 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 
+    group-hover:-translate-x-40 ease ${disabled || loading ? "hidden" : ""}
+  `;
+
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled || loading}
+      className={buttonClasses}
+    >
+      <span className={spanClasses}></span>
+      <span className="relative flex items-center justify-center text-base font-semibold">
+        {loading ? (
+          <>
+            <svg
+              className="w-5 h-5 mr-2 text-white animate-spin"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            {title}
+          </>
+        ) : (
+          title
+        )}
+      </span>
+    </button>
+  );
+}
+
+export default Button;
---
+++ backend/templates/display/SideDrawer.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import { XMarkIcon, Bars3Icon } from "@heroicons/react/24/outline"; // You can use any icons you like
+import ConnectWalletTemplate from "../connection/ConnectWalletTemplate";
+
+function SideDrawer({ isOpen, onClose }) {
+  return (
+    <div
+      className={`fixed inset-0 z-50 flex ${
+        isOpen ? "translate-x-0" : "-translate-x-full"
+      } transition-transform duration-300 bg-neutral-900 bg-opacity-75`}
+    >
+      <div className="relative flex flex-col w-64 h-full p-4 shadow-lg bg-neutral-800">
+        <button className="flex justify-end text-white" onClick={onClose}>
+          <XMarkIcon className="w-6 h-6" />
+        </button>
+        <ConnectWalletTemplate />
+        <nav className="flex flex-col p-6 space-y-4">
+          <Link href="/" passHref>
+            <p className="text-white hover:text-gray-400" onClick={onClose}>
+              Home
+            </p>
+          </Link>
+          <Link href="/swap" passHref>
+            <p className="text-white hover:text-gray-400" onClick={onClose}>
+              Swap
+            </p>
+          </Link>
+          <Link href="/transfer" passHref>
+            <p className="text-white hover:text-gray-400" onClick={onClose}>
+              Transfer History
+            </p>
+          </Link>
+        </nav>
+      </div>
+      <div className="flex-grow" onClick={onClose} />
+    </div>
+  );
+}
+
+export default SideDrawer;
