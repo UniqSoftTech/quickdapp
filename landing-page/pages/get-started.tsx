c8193fbfa5939ef9268c4b5726630aae198c418c
Batgerelt <grey@uniqsoft.tech>
2024-08-06 23:07:47 +0800

    Update get-started.tsx

--- landing-page/pages/get-started.tsx
+++ landing-page/pages/get-started.tsx
@@ -29,18 +29,11 @@ export default function GetStartedPage() {
   async function handleSubmitContractAddress(e: any) {
     e.preventDefault();
 
-    const res = await fetchRequest({
-      url: "/contract/get-metadata",
-      method: "POST",
-      model: "ContractMetaData",
-      body: { address: contractAddress },
-    });
-
-    if (res?.success) {
-      return setStep(2);
+    if (!ethers.isAddress(contractAddress)) {
+      return showToast("Contract address is valid", "error");
     }
 
-    showToast(res?.messase, "error");
+    return setStep(2);
   }
 
   async function handleSubmit(e: any) {
@@ -52,8 +45,8 @@ export default function GetStartedPage() {
       model: "GettingStart",
       body: {
         contract_address: contractAddress,
-        title: generalConfig.title,
         description: generalConfig.desc,
+        title: generalConfig.title,
         theme: generalConfig.theme,
         logo: generalConfig.logo,
       },
@@ -83,93 +76,77 @@ export default function GetStartedPage() {
     <div className="flex min-h-screen flex-col items-center justify-center bg-black overflow-hidden font-mono">
       <div className="absolute top-0 left-0 w-full h-full bg-gradient-radial from-blue-800 via-transparent to-black opacity-50 pointer-events-none"></div>
 
-      <div className="w-6/12 border bg-black rounded-2xl shadow-2xl p-4 flex flex-col gap-6">
+      <div className="w-11/12 sm:w-10/12 md:w-6/12 lg:w-4/12 xl:w-3/12 border bg-black rounded-2xl shadow-2xl p-4 flex flex-col gap-6">
         <p className="text-white text-center font-bold text-2xl">
           Customize your Application
         </p>
 
         {step === 1 && <ContractAddressStep />}
         {step === 2 && (
-          <div className="flex flex-col gap-4">
-            <div className="">
-              <div className="grid grid-cols-2 border p-2">
-                <label className="text-white">Name:</label>
-                <label className="text-white">
-                  {resContractMetaData?.name}
-                </label>
+          <form onSubmit={handleSubmit} className="w-100 flex flex-col gap-3">
+            <label className="text-white">App Title:</label>
+            <input
+              type="text"
+              placeholder="Enter app title"
+              className="p-2 border outline-none"
+              value={generalConfig.title}
+              onChange={(e) =>
+                setGeneralConfig((prev) => ({
+                  ...prev,
+                  title: e.target.value,
+                }))
+              }
+            />
 
-                <label className="text-white">Symbol:</label>
-                <label className="text-white">
-                  {resContractMetaData?.symbol}
-                </label>
-              </div>
-            </div>
+            <label className="text-white">App description:</label>
+            <input
+              type="text"
+              placeholder="Enter app description"
+              className="p-2 border outline-none"
+              value={generalConfig.desc}
+              onChange={(e) =>
+                setGeneralConfig((prev) => ({
+                  ...prev,
+                  desc: e.target.value,
+                }))
+              }
+            />
 
-            <form onSubmit={handleSubmit} className="w-100 flex flex-col gap-3">
-              <label className="text-white">App Title:</label>
-              <input
-                type="text"
-                placeholder="Enter app title"
-                className="p-2 border"
-                value={generalConfig.title}
-                onChange={(e) =>
-                  setGeneralConfig((prev) => ({
-                    ...prev,
-                    title: e.target.value,
-                  }))
-                }
-              />
+            <label className="text-white">Logo URL:</label>
+            <input
+              type="text"
+              placeholder="Enter logo URL"
+              className="p-2 border outline-none"
+              value={generalConfig.logo}
+              onChange={(e) =>
+                setGeneralConfig((prev) => ({
+                  ...prev,
+                  logo: e.target.value,
+                }))
+              }
+            />
 
-              <label className="text-white">App description:</label>
-              <input
-                type="text"
-                placeholder="Enter app description"
-                className="p-2 border"
-                value={generalConfig.desc}
-                onChange={(e) =>
-                  setGeneralConfig((prev) => ({
-                    ...prev,
-                    desc: e.target.value,
-                  }))
-                }
-              />
+            <label className="text-white">Primary color:</label>
+            <input
+              type="color"
+              placeholder="Enter logo URL"
+              className="w-full outline-none"
+              color={generalConfig.color}
+              onChange={(e) =>
+                setGeneralConfig((prev) => ({
+                  ...prev,
+                  color: e.target.value,
+                }))
+              }
+            />
 
-              <label className="text-white">Logo URL:</label>
-              <input
-                type="text"
-                placeholder="Enter logo URL"
-                className="p-2 border"
-                value={generalConfig.logo}
-                onChange={(e) =>
-                  setGeneralConfig((prev) => ({
-                    ...prev,
-                    logo: e.target.value,
-                  }))
-                }
-              />
-
-              <label className="text-white">Primary color:</label>
-              <input
-                type="color"
-                placeholder="Enter logo URL"
-                className="w-full"
-                color={generalConfig.color}
-                onChange={(e) =>
-                  setGeneralConfig((prev) => ({
-                    ...prev,
-                    color: e.target.value,
-                  }))
-                }
-              />
-
-              <button
-                type="submit"
-                className="p-2 border flex flex-row justify-center items-center text-white"
-              >
-                {isGettingStart ? <Spinner /> : "Generate Dapp"}
-              </button>
-            </form>
-          </div>
+            <button
+              type="submit"
+              className="p-2 mt-4 border flex flex-row justify-center items-center text-white"
+            >
+              {isGettingStart ? <Spinner /> : "Generate Dapp"}
+            </button>
+          </form>
         )}
       </div>
     </div>
