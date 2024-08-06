dedc6c5a24f73c467ebc708e6db187a3a247b03d
Batgerelt <grey@uniqsoft.tech>
2024-08-06 23:26:39 +0800

    added route contract/get-abi

--- backend/src/controller/contract.controller.ts
+++ backend/src/controller/contract.controller.ts
@@ -3,12 +3,15 @@ import { Response, Request } from "express";
 import { failed, success } from "../utils/res.utils";
 import { ethers } from "ethers"
 import { AlchemyService } from '../services/alchemy.service';
+import { BlockscoutService } from '../services/blocksout.service';
 
 export class ContractController {
   private alchemyService: AlchemyService;
+  private blocksoutService: BlockscoutService;
 
   constructor() {
     this.alchemyService = new AlchemyService();
+    this.blocksoutService = new BlockscoutService();
   }
 
   /**
@@ -22,6 +25,9 @@ export class ContractController {
     try {
       const { address, network } = req.body;
 
+      if(!ethers.isAddress(address))
+        return failed({ res, message: "Invalid address" });
+
       const result = await this.alchemyService.getTokenMetadata(address, network);
       success({ res, result })
     } catch (error: Error | any) {
@@ -29,12 +35,25 @@ export class ContractController {
     }
   }
 
-
   async getTopTokens(req: Request, res: Response): Promise<void> {
     try {
       success({ res, result: topTokens });
     } catch (error: Error | any) {
       failed({ res, err: error, message: `Failed to get top tokens` });
+    }
+  }
+
+  async getABI(req: Request, res: Response): Promise<void> {
+    try {
+      const { address } = req.body;
+
+      if(!ethers.isAddress(address))
+        return failed({ res, message: "Invalid address" });
+
+      const result = await this.blocksoutService.getABI(address);
+      success({ res, result });
+    } catch (error: Error | any) {
+      failed({ res, err: error, message: `Failed to get ABI for contract ${req.body.address}` });
     }
   }
 }
