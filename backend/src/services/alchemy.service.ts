f7a263f8c130c8bd0597ad84e48f06788bf0dd05
Batgerelt <grey@uniqsoft.tech>
2024-08-07 10:31:34 +0800

    ci/cd test

--- backend/src/services/alchemy.service.ts
+++ backend/src/services/alchemy.service.ts
@@ -13,15 +13,6 @@ export class AlchemyService {
   constructor() {
     this.alchemy = new Alchemy(config);
   }
-
-  /**
-   * Retrieves the metadata of a token contract.
-   *
-   * @param {string} address - The address of the token contract.
-   * @param {Network} network - The network on which the token contract is deployed.
-   * @return {Promise<TokenMetadataResponse>} A promise that resolves to the token metadata response.
-   * @throws {Error} If there is an error retrieving the token metadata.
-   */
   async getTokenMetadata(address: string, network?: Network): Promise<TokenMetadataResponse | null> {
     try {
       const { name, symbol, decimals, logo }: TokenMetadataResponse = await this.alchemy.core.getTokenMetadata(address);
